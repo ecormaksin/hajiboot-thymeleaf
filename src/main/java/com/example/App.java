6dedc2829d5d9e3dee70af787cd1cea39c3b8409
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-24 20:08:09 +0900

    [2.3.2] 「Sring Data JPA」を使った「リポジトリ・クラス」の作成

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,31 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.domain.customer.Customer;
+import com.example.domain.customer.CustomerRepository;
+
+@SpringBootApplication
+public class App implements CommandLineRunner {
+	CustomerRepository customerRepository;
+
+	App(CustomerRepository aCustomerRepository) {
+		this.customerRepository = aCustomerRepository;
+	}
+
+	@Override
+	public void run(String... strings) throws Exception {
+		// データ追加
+		Customer created = customerRepository.save(new Customer(null, "Hidetoshi", "Dekisugi"));
+		System.out.println(created + " is created!");
+		// データ表示
+		customerRepository.findAll()
+			.forEach(System.out::println);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+}
