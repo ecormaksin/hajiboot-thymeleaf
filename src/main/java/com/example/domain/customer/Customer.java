c5f22da9f16c2c06ef0551a83f0b3f80c96a66a9
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-24 19:58:27 +0900

    [2.3.1] 「JPA」の「エンティティ・クラス」の作成

---
+++ src/main/java/com/example/domain/customer/Customer.java
@@ -0,0 +1,26 @@
+package com.example.domain.customer;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Entity
+@Table(name = "customers")
+@Data
+@AllArgsConstructor
+public class Customer {
+	@Id
+	@GeneratedValue
+	private Integer id;
+	@Column(nullable = false)
+	private String firstName;
+	@Column(nullable = false)
+	private String lastName;
+
+	protected Customer() {}
+}
---
+++ src/main/java/com/example/domain/customer/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * 顧客に関するドメインモデルを格納する
+ */
+package com.example.domain.customer;
