84e517b882a77033f18ec8e98c803b410a04025b
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-03-19 23:16:50 +0900

    4.5 「Heroku」上のPostgreSQLを使用

---
+++ src/main/java/com/example/AppConfig.java
@@ -0,0 +1,54 @@
+package com.example;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import javax.sql.DataSource;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.RequiredArgsConstructor;
+import net.sf.log4jdbc.sql.jdbcapi.DataSourceSpy;
+
+@Configuration
+@RequiredArgsConstructor
+public class AppConfig {
+
+	private final DataSourceProperties properties;
+	DataSource dataSource;
+	
+	@Bean(destroyMethod = "close")
+	DataSource realDataSource() throws URISyntaxException {
+		String url;
+		String username;
+		String password;
+		
+		String databaseUrl = System.getenv("DATABASE_URL");
+		if (databaseUrl != null) {
+			URI dbUri = new URI(databaseUrl);
+			url = "jdbc:postgresql://" + dbUri.getHost() + dbUri.getPath() + ":" + dbUri.getPort() + dbUri.getPath();
+			username = dbUri.getUserInfo().split(":")[0];
+			password = dbUri.getUserInfo().split(":")[1];
+		} else {
+			url = this.properties.getUrl();
+			username = this.properties.getUsername();
+			password = this.properties.getPassword();
+		}
+		
+		DataSourceBuilder<?> factory = DataSourceBuilder
+				.create(this.properties.getClassLoader())
+				.url(url)
+				.username(username)
+				.password(password);
+		this.dataSource = factory.build();
+		return this.dataSource;
+	}
+
+	@Bean
+	DataSource dataSource() {
+		return new DataSourceSpy(this.dataSource);
+	}
+}
