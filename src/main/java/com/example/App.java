edb2fdf3f8250a9cda691712d59cffad068d7826
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-03-21 17:16:27 +0900

    値の有無に拘わらず変数名を出力するよう変更

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,23 +12,24 @@ public class App {
 
 	@GetMapping("/home")
 	String home() {
-		outputEnvVar("DATABASE_URL");
-		outputEnvVar("JDBC_DATABASE_URL");
-		outputEnvVar("JDBC_DATABASE_USERNAME");
-		outputEnvVar("JDBC_DATABASE_PASSWORD");
-		outputEnvVar("SPRING_DATASOURCE_URL");
-		outputEnvVar("SPRING_DATASOURCE_USERNAME");
-		outputEnvVar("SPRING_DATASOURCE_PASSWORD");
+		StringBuffer sb = new StringBuffer();
+		
+		sb.append( getEnvVar("DATABASE_URL") );
+		sb.append( getEnvVar("JDBC_DATABASE_URL") );
+		sb.append( getEnvVar("JDBC_DATABASE_USERNAME") );
+		sb.append( getEnvVar("JDBC_DATABASE_PASSWORD") );
+		sb.append( getEnvVar("SPRING_DATASOURCE_URL") );
+		sb.append( getEnvVar("SPRING_DATASOURCE_USERNAME") );
+		sb.append( getEnvVar("SPRING_DATASOURCE_PASSWORD") );
 
-		return "Test";
+		return "Test " + sb.toString();
 	}
 	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
 
-	private static void outputEnvVar(String key) {
-		if (null == System.getenv(key)) return;
-		System.out.println(key + ": " + System.getenv(key));
+	private static String getEnvVar(String key) {
+		return key + ": " + StringUtils.defaultString(System.getenv(key), "unavailable");
 	}
 }
