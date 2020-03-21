80fab93e753958b66c155580da69dd353547ce2c
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-03-21 14:22:58 +0900

    シンプルな形でデバッグ

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,11 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
+	@GetMapping("/home")
+	String home() {
 		outputEnvVar("DATABASE_URL");
 		outputEnvVar("JDBC_DATABASE_URL");
 		outputEnvVar("JDBC_DATABASE_USERNAME");
@@ -15,7 +19,11 @@ public class App {
 		outputEnvVar("SPRING_DATASOURCE_USERNAME");
 		outputEnvVar("SPRING_DATASOURCE_PASSWORD");
 
-//		SpringApplication.run(App.class, args);
+		return "Test";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
 	}
 
 	private static void outputEnvVar(String key) {
